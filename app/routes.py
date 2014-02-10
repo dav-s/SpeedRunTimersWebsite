d354e291a16377520737fd78b9c4e161b86e7911
Davis Robertson <EpicDaviBrickFilms@gmail.com>
2014-02-10 00:17:43 -0600

    Changing Error Pages
    Made one template for all error pages ("errorpages.html"). Error is now
    passed through "routes.py".

--- app/routes.py
+++ app/routes.py
@@ -16,14 +16,28 @@ navigationBar = [{
 def injectNav():
 	return dict(nBar = navigationBar)
 
-@app.errorhandler(404)
-def fofPage(e):
-	return render_template("404page.html", title="Not found, Bro!")
 
+# Error Pages
 @app.errorhandler(401)
 def fooPage(e):
-	return render_template("401page.html", title="Unauthorized, Bro!")
+	return render_template("errorpage.html", title="Unauthorized, Bro!",
+		mainMess="You don't have the authorization to this page!",
+		sideMess="You might not be logged in or are snooping in a place you shouldn't!")
 
+@app.errorhandler(403)
+def fotPage(e):
+	return render_template("errorpage.html", title="Forbidden, Bro!",
+		mainMess="You don't have permission to view this page!",
+		sideMess="Please don't snoop around!")
+
+@app.errorhandler(404)
+def fofPage(e):
+	return render_template("errorpage.html", title="Not Found, Bro!",
+		mainMess="This page doesn't exist!",
+		sideMess="You might of typed in the wrong url, or just stupid.")
+
+
+# Regular Routes
 @app.route("/")
 def index():
 	return render_template("index.html")
